39e03b43f8b3b358b972c444020c4102cf4b45eb
Alex Just <alexjustdata@gmail.com>
2024-07-10 12:28:29 +0200

    upload readme Dimensions

---
+++ dbt_learn/models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,14 @@
+(dbt_env) ➜  dbt_learn git:(main) ✗ cat models/dim/dim_hosts_cleansed.sql
+-- models/dim/dim_hosts_cleansed.sql
+
+WITH src_hosts AS (
+    SELECT * FROM {{ ref('src_hosts') }}
+)
+SELECT 
+    host_id,
+    NVL(host_name, 'Anonymous') AS host_name,
+    is_superhost,
+    created_at,
+    updated_at
+FROM
+    src_hosts
